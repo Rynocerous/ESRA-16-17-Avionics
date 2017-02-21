ea9a1de46a03040bab36bafd0d203d3fef27f21a
Rynocerous <ryan.domres@gmail.com>
2017-02-20 16:30:22 -0800

    GPIO and USART2 initialiation moved to functions.
    The initialization code that has been written up to this point was
    segmented into more descriptive functions.

--- src/main.c
+++ src/main.c
@@ -1,34 +1,5 @@
-/**
-  ******************************************************************************
-  * @file    GPIO/IOToggle/main.c
-  * @author  MCD Application Team
-  * @version V3.5.0
-  * @date    08-April-2011
-  * @brief   Main program body.
-  ******************************************************************************
-  * @attention
-  *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
-  ******************************************************************************
-  */ 
-
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
-
-/** @addtogroup STM32F10x_StdPeriph_Examples
-  * @{
-  */
-
-/** @addtogroup GPIO_IOToggle
-  * @{
-  */
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
@@ -40,95 +11,86 @@ uint8_t x;
 
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
-void Delay(uint32_t nCount) {
+void delay(uint32_t nCount) {
 	while (nCount > 0) {
 			nCount--;
 	}
 }
 
-/**
-  * @brief  Main program.
-  * @param  None
-  * @retval None
-  */
+void initGPIO() {
+	/* GPIOC Periph clock enable */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+	/* Configure LEDs in output pushpull mode */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+}
+
+void initXBee() {
+	/* USART2 Peripheral clock enable */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+
+	/* Configure USART2 Rx as input floating */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure USART2 Tx as alternate function push pull */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* USART2 configuration ------------------------------------------------------*/
+	/* USART2 configured as follow:
+		- BaudRate = 230400 baud
+		- Word Length = 8 Bits
+		- One Stop Bit
+		- Even parity
+		- Hardware flow control disabled (RTS and CTS signals)
+		- Receive and transmit enabled
+	*/
+	USART_InitStructure.USART_BaudRate = 230400;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_Even;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+	/* Configure USART2 */
+	USART_Init(USART2, &USART_InitStructure);
+
+	/* Enable USART2 */
+	USART_Cmd(USART2, ENABLE);
+}
+
 int main(void)
 {
-  /*!< At this stage the microcontroller clock setting is already configured, 
-       this is done through SystemInit() function which is called from startup
-       file (startup_stm32f10x_xx.s) before to branch to application main.
-       To reconfigure the default setting of SystemInit() function, refer to
-       system_stm32f10x.c file
-     */     
-       
-  /* GPIOC Periph clock enable */
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 
-  /* Configure PD0 and PD2 in output pushpull mode */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
+	initGPIO();
 
-  /* USART Periph clock enable */
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+	initXBee();
 
-  /* Configure USART2 Rx as input floating */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+	while (1)
+	{
+		/* Set PC0 and PC2 */
+		GPIOC->BSRR = 0x00000001;
+		GPIOC->BRR  = 0x00000004;
+		delay(100000);
+		/* Set PC1 and PC3 */
+		GPIOC->BSRR = 0x00000004;
+		GPIOC->BRR  = 0x00000001;
+		//delay(500000);
 
-  /* Configure USART2 Tx as alternate function push pull */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-  /* USART2 configuration ------------------------------------------------------*/
-    /* USART2 configured as follow:
-          - BaudRate = 230400 baud
-          - Word Length = 8 Bits
-          - One Stop Bit
-          - Even parity
-          - Hardware flow control disabled (RTS and CTS signals)
-          - Receive and transmit enabled
-    */
-    USART_InitStructure.USART_BaudRate = 230400;
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;
-    USART_InitStructure.USART_Parity = USART_Parity_Even;
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-    /* Configure USARTy */
-    USART_Init(USART2, &USART_InitStructure);
-
-    /* Enable the USARTy */
-    USART_Cmd(USART2, ENABLE);
-
-  /* To achieve GPIO toggling maximum frequency, the following  sequence is mandatory.
-     You can monitor PD0 or PD2 on the scope to measure the output signal.
-     If you need to fine tune this frequency, you can add more GPIO set/reset
-     cycles to minimize more the infinite loop timing.
-     This code needs to be compiled with high speed optimization option.  */
-  while (1)
-  {
-    /* Set PC0 and PC2 */
-    GPIOC->BSRR = 0x00000001;
-    GPIOC->BRR  = 0x00000004;
-    Delay(100000);
-    /* Set PC1 and PC3 */
-    GPIOC->BSRR = 0x00000004;
-    GPIOC->BRR  = 0x00000001;
-    //Delay(500000);
-
-    /* Loop until the USART2 Receive Data Register is not empty */
-    while(USART_GetFlagStatus(USART2, USART_FLAG_RXNE) == RESET)
-    {
-    }
-    x = USART_ReceiveData(USART2);
-    USART_SendData(USART2, x);
-  }
+		/* Loop until the USART2 Receive Data Register is not empty */
+		while(USART_GetFlagStatus(USART2, USART_FLAG_RXNE) == RESET){}
+		x = USART_ReceiveData(USART2);
+		/* Send received data back out on USART2 */
+		USART_SendData(USART2, x);
+	}
 }
 
 #ifdef  USE_FULL_ASSERT
@@ -152,13 +114,3 @@ void assert_failed(uint8_t* file, uint32_t line)
 }
 
 #endif
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
